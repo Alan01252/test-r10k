b157211b36298ef5a3d0d7c734d07149969c21a7
Alan Hollis <me@alanhollis.com>
2017-08-12 15:54:19 +0100

    Change node name

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,5 @@
- node "centos.foss.psick.io" {
+node centos6 {
+
   class { "::ntp":
     servers    => [ '0.us.pool.ntp.org iburst','1.us.pool.ntp.org iburst','2.us.pool.ntp.org iburst','3.us.pool.ntp.org iburst'],
 	restrict => ['127.0.0.1']
