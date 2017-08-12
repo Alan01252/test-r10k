0e5e3a0c7f281f6f16e974438b359043fd7776db
Alan Hollis <me@alanhollis.com>
2017-08-12 16:06:13 +0100

    Test pushing some modules
    Not sure why without doing this the modules dir isn't being synced

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@ node centos6 {
 
   class { "::ntp":
     servers    => [ '0.us.pool.ntp.org iburst','1.us.pool.ntp.org iburst','2.us.pool.ntp.org iburst','3.us.pool.ntp.org iburst'],
-	restrict => ['127.0.0.1']
+	restrict => ['127.0.0.11']
   }
  }
